4eae055aa8652264df71c4fa734f05c868b0ad9a
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-09-22 20:42:07 +0300

    adding styles and fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+});
